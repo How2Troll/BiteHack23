8d0dd12f7c7f64ebcd19422ab4bc1c0af1cbf854
kierzek <kierzek@student.agh.edu.pl>
2023-01-14 14:51:48 +0100

    Init

---
+++ main/main.ino
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+
+
+void app_main(){
+  nvs_flash_init();
+  xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+}
+
+void setup(){
+  app_main();
+}
+
+void loop(){
+  
+}
+
+
+void hello_task(void *pvParameter)
+{
+}
