b8e622ec4acd31b60ca30e6d53dd3d7a267d1bb5
kierzek <kierzek@student.agh.edu.pl>
2023-01-14 19:22:34 +0100

    folder structure

---
+++ main/main.ino
@@ -0,0 +1,19 @@
+/*
+
+To include external file, add *.hpp file to inc directory. 
+Source files are stored in directory main
+example:
+#include "inc/<your_file>.hpp"
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
